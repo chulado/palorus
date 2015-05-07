09b3b699bcbc3c658126bd356544ae22f4026a14
Conrad VanLandingham <cv@utexas.edu>
2015-05-07 10:52:52 -0500

    some scrollmagic and css tweaks

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,19 +1,88 @@
-// var options = {
-//   horizontal: 1,
-//   itemNav: 'forceCentered',
-//   smart: 1,
-//   activateMiddle: 1,
-//   activateOn: 'click',
-//   mouseDragging: 1,
-//   touchDragging: 1,
-//   releaseSwing: 1,
-//   startAt: 4,
-//   scrollBy: 1,
-//   speed: 300,
-//   elasticBounds: 1,
-//   dragHandle: 1,
-//   dynamicHandle: 1,
-//   clickBar: 1
+$(document).ready(function() {
 
-// };
-// var frame = new Sly('.frame', options).init();
+  var options = {
+    horizontal: 1,
+    itemNav: 'forceCentered',
+    smart: 1,
+    activateMiddle: 1,
+    scrollSource: null,
+    activateOn: 'click',
+    mouseDragging: 1,
+    touchDragging: 1,
+    releaseSwing: 1,
+    startAt: 0,
+    scrollBy: 1,
+    speed: 300,
+    elasticBounds: 1,
+    dragHandle: 1,
+    dynamicHandle: 1,
+    clickBar: 1
+
+  };
+  var frame = new Sly('.frame', options).init();
+
+  // init controller
+  var controller = new ScrollMagic.Controller();
+
+  new ScrollMagic.Scene({
+    triggerElement: ".hero-giftright",
+    triggerHook: "onLeave",
+    duration: $(".hero-giftright").height()*1.5
+  })
+  .setTween(TweenMax.fromTo(
+    ".hero-giftright .hero-bg", 1,
+    { y: 0, z: 0.1 },
+    { y: "30%", z: 0.1 }
+  ))
+  .addTo(controller);
+
+
+  new ScrollMagic.Scene({
+    triggerElement: ".hero-nevermiss",
+    triggerHook: "onEnter",
+    duration: $(".hero-nevermiss").height()*3
+  })
+  .setTween(TweenMax.fromTo(
+    ".hero-nevermiss .hero-bg", 1,
+    { y: "-25%", z: 0.1 },
+    { y: "10%", z: 0.1 }
+  ))
+  .addTo(controller);
+
+
+  new ScrollMagic.Scene({
+    triggerElement: ".hero-deserveit",
+    triggerHook: "onEnter",
+    duration: $(".hero-deserveit").height()*3
+  })
+  .setTween(TweenMax.fromTo(
+    ".hero-deserveit .hero-bg", 1,
+    { y: "-25%", z: 0.1 },
+    { y: "10%", z: 0.1 }
+  ))
+  .addTo(controller);
+
+
+
+  new ScrollMagic.Scene({
+    triggerElement: ".hero-dothis",
+    triggerHook: "onEnter",
+    duration: $(".hero-dothis").height()*3
+  })
+  .setTween(TweenMax.fromTo(
+    ".hero-dothis .hero-bg", 1,
+    { y: "-25%", z: 0.1 },
+    { y: "10%", z: 0.1 }
+  ))
+  .addTo(controller);
+
+
+  $(window).on("scroll", function() {
+    if($(window).scrollTop()>1) {
+      $("body").addClass("scrolled");
+    } else {
+      $("body").removeClass("scrolled");
+    }
+  });
+
+});
