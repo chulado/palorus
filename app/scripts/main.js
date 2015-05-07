de37b0f2811797207b82971b039a534208040633
Conrad VanLandingham <cv@utexas.edu>
2015-05-07 00:43:45 -0500

    first commit

---
+++ app/scripts/main.js
@@ -0,0 +1,19 @@
+// var options = {
+//   horizontal: 1,
+//   itemNav: 'forceCentered',
+//   smart: 1,
+//   activateMiddle: 1,
+//   activateOn: 'click',
+//   mouseDragging: 1,
+//   touchDragging: 1,
+//   releaseSwing: 1,
+//   startAt: 4,
+//   scrollBy: 1,
+//   speed: 300,
+//   elasticBounds: 1,
+//   dragHandle: 1,
+//   dynamicHandle: 1,
+//   clickBar: 1
+
+// };
+// var frame = new Sly('.frame', options).init();
